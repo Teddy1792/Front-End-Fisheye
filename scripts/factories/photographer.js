e06a45c2359cf2cb74baf5316bb33a2f40b1ec74
Theodore Abitbol <106872220+Teddy1792@users.noreply.github.com>
2023-03-14 10:41:03 +0100

    pages linked and index ready

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,49 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price, id } = data;
     const picture = `assets/photographers/${portrait}`;
 
+//génération des éléments du DOM
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "portrait du ou de la photograhe");
+
+
+//création du lien vers les pages perso des photographes
+        const photographerId = id; // stockage de la data id dans une constante
+        const url = new URL('photographer.html', window.location.href); // création de l'objet URL
+        url.searchParams.set('id', photographerId); // assignation de la valeur id
+        console.log(url.href); // sortie: .../photographer.html?id=id
+
+//creation du link <a> contenant le lien vers les pages photographers
+        const link = document.createElement( 'a' );
+        link.setAttribute("class", "lien");
+        link.setAttribute("aria-label", `lien vers la page perso de ${name}`);
+        link.setAttribute("href", url.href);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const locationElement = document.createElement ('p');
+        locationElement.classList.add("location");
+        locationElement.innerText = `${city}, ${country}`;
+
+        const taglineElement = document.createElement ('p');
+        taglineElement.classList.add("line");
+        taglineElement.innerText = `${tagline}`;
+
+        const priceElement = document.createElement ('p');
+        priceElement.classList.add("price");
+        priceElement.innerText = `${price}€‎/jours`;
+
+//rattachement des éléments du DOM
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(locationElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,90 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function filterPhotographers(data) {
+    const response = await fetch('../../data/photographers.json');
+    const parsedData = await response.json();
+    const photographers = parsedData.photographers;
+    const filteredPhotographers = photographers.filter(function(photographer) {
+        return photographer.id.toString() === data;
+        });
+    return (filteredPhotographers[0]);
+}
+
+function displayPhotographer (data) {
+
+    //générer la box
+    const container = document.createElement('div');
+    container.classList.add("conteneur");
+    document.querySelector('.photograph-header').appendChild(container);
+
+
+    //display le nom
+    const { name, portrait, city, country, tagline, price, id } = data;
+    const nom = document.createElement('p');
+    nom.classList.add("nom");
+    nom.textContent = `${name}`;
+    container.appendChild(nom);
+
+    //display le lieu
+    const locationElement = document.createElement ('p');
+    locationElement.classList.add("location");
+    locationElement.innerText = `${city}, ${country}`;
+    container.appendChild(locationElement);
+
+    //display la tagline
+    const taglineElement = document.createElement ('p');
+    taglineElement.classList.add("line");
+    taglineElement.innerText = `${tagline}`;
+    container.appendChild(taglineElement);
+
+    //display photo
+    const img = document.createElement( 'img' );
+    img.setAttribute("src", `assets/photographers/${portrait}`);
+    img.setAttribute("alt", "portrait du ou de la photograhe");
+    document.querySelector('.photograph-header').appendChild(img);
+
+    //display menu de tri
+    const main = document.getElementById("main");
+
+    const conteneurTri = document.createElement("div");
+    conteneurTri.classList.add("conteneurTri");
+
+    const buttonText = document.createElement("p");
+    buttonText.classList.add("buttonText");
+    buttonText.innerHTML = "Trier par";
+
+    const buttonLabel = document.createElement("label");
+    buttonLabel.classList.add("label");
+    buttonLabel.innerHTML = "Popularité";
+
+    const triButton = document.createElement("button");
+    triButton.setAttribute("multiple", "");
+    triButton.setAttribute("role", "listbox");
+    triButton.classList.add("dropdownButton");
+    
+
+    const popularite = document.createElement("option");
+    popularite.innerHTML = "Popularité";
+    const Date = document.createElement("option");
+    Date.innerHTML = "Date";
+    const Titre = document.createElement("option");
+    Titre.innerHTML = "Titre";
+
+
+    main.appendChild(conteneurTri);
+    conteneurTri.appendChild(buttonText);
+    conteneurTri.appendChild(buttonLabel);
+    conteneurTri.appendChild(triButton);
+    triButton.appendChild(popularite);
+    triButton.appendChild(Date);
+    triButton.appendChild(Titre);
+    buttonLabel.appendChild(triButton);
+}
+
+async function init() {
+    const url = new URL(window.location.href);
+    let photographerId = url.searchParams.get("id");
+    let selectedPhotographer = await filterPhotographers(photographerId);
+    displayPhotographer(selectedPhotographer);
+};
+
+init();
